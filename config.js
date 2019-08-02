17d48b3b4c4b27dfc772139187d2e8f2891ab711
midhun1995 <midhunnair95@gmail.com>
2019-08-02 15:37:44 +0530

    iitial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+
+//427b90044ede46d28b8e293f54bea788
+
+//60da65e2782f4c1ab12a48a2f56cdc3d
+
+//https://api.dialogflow.com/v1/query?v=20150910
+
+module.exports = {
+    
+    configuration:{
+        chatserver: 'https://api.dialogflow.com/v1/query',
+        clientAccessToken: 'b8cf164d86734f98a2334b86b10f3d28'
+    }
+    
+    
+}
